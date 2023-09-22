986de33ff1df77196bbd154e05f11783e1e386b1
MangilipellyKavya <131743696+MangilipellyKavya@users.noreply.github.com>
2023-09-22 22:42:48 +0530

    Add files via upload

---
+++ filehandling/WriteToFile.java
@@ -0,0 +1,24 @@
+package com.svit.filehandling;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToFile {
+	  public static void main(String[] args)
+	  {
+			try {
+				
+			      FileWriter myWriter = new FileWriter("file.txt");
+			      myWriter.write("This is our First Task");
+			      myWriter.close();
+			      System.out.println("Successfully wrote to the file.");
+
+				
+			} catch (IOException e) {
+			      System.out.println("An error occurred.");
+			      e.printStackTrace();
+
+			}
+		}
+
+}
